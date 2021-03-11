081831c662f49c4ddc97c2d5c75b5a0b27006503
DSD-18 <72644829+DSD-18@users.noreply.github.com>
2021-03-11 15:46:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function setup() {
+    createCanvas(1300,1300);
+    }
+    
+    function draw() {
+    circle (50,50,50)
+    circle (50,550,50)
+    circle (1250,50,50)
+    circle (1250,550,50)
+    
+    }
